154fd2aa421f4b1a7826aa908d14361ab3bdf0be
Dima <d.i.orekhov@gmail.com>
2021-11-12 01:10:09 +0300

    main

--- pu_graphs/main.py
+++ pu_graphs/main.py
@@ -1,9 +1,12 @@
+import os
+from functools import partial
 from pathlib import Path
 
 import dgl
 import torch
 from catalyst.engines import DeviceEngine
 from catalyst import dl
+from catalyst.metrics import reciprocal_rank, mrr, MRRMetric, AccuracyMetric
 from catalyst.utils.config import load_config
 from catalyst.utils import set_global_seed
 from torch.utils.data import DataLoader
@@ -11,13 +14,18 @@ from torch.utils.data import DataLoader
 from pu_graphs.data.datasets import DglGraphDataset
 from pu_graphs.data.negative_sampling import UniformStrategy
 from pu_graphs.data.utils import get_split
-from pu_graphs.evaluation.evaluation import compute_mrr_for_loader
+from pu_graphs.debug_utils import DebugDataset
+from pu_graphs.evaluation.evaluation import compute_score_based_metrics_for_loader, MRRLinkPredictionMetric, \
+    AccuracyLinPredictionMetric
 from pu_graphs.modeling.dist_mult import DistMult
 from pu_graphs.modeling.loss import UnbiasedPULoss, sigmoid_loss, logistic_loss
 
 
 def main():
     config = load_config("config.yaml")  # FIXME: replace hardcode
+
+    if config["is_debug"]:
+        os.environ["WANDB_MODE"] = "dryrun"
 
     set_global_seed(config["seed"])
 
@@ -36,8 +44,15 @@ def main():
         for split_key, graph in graphs.items()
     }
 
+    if config["is_debug"]:
+        datasets = {k: DebugDataset(v, n_examples=100) for k, v in datasets.items()}
+
     loaders = {
-        split_key: DataLoader(dataset, batch_size=config["batch_size"], shuffle=True)
+        split_key: DataLoader(
+            dataset,
+            batch_size=config["batch_size"] if split_key == "train" else config["eval_batch_size"],
+            shuffle=True
+        )
         for split_key, dataset in datasets.items()
     }
     train_and_valid_loaders = {
@@ -77,6 +92,10 @@ def main():
         )
     ]  # TODO: init
 
+    loggers = {
+        "wandb": dl.WandbLogger(project="pu_graphs", name=config["run_name"])
+    }
+
     runner = dl.SupervisedRunner(
         input_key=["head_indices", "tail_indices"],
         output_key="logits",
@@ -95,17 +114,23 @@ def main():
         logdir=Path("./logdir") / config["run_name"],
         num_epochs=config["num_epochs"],
         check=config["is_debug"],
-        load_best_on_end=True
+        load_best_on_end=True,
+        hparams=config,
+        loggers=loggers
     )
 
     # TODO: move inside runner
     for split_key in ("valid", "test"):
-        mrr = compute_mrr_for_loader(
+        metrics = compute_score_based_metrics_for_loader(
             graph=graphs[split_key],
-            model=runner.model,
-            loader=loaders[split_key]
+            model=model,
+            loader=loaders[split_key],
+            metrics={
+                "mrr": MRRLinkPredictionMetric(topk_args=[full_graph.number_of_nodes()]),
+                "acc": AccuracyLinPredictionMetric(topk_args=[1, 3, 5, 10, 20])
+            }
         )
-        print(split_key, mrr)
+        print(split_key, metrics)
 
 
 if __name__ == '__main__':
