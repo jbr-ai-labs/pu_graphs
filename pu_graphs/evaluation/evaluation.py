09595c85b9cb821f08c7bd46e860f28e0f52648d
Dima <d.i.orekhov@gmail.com>
2021-11-17 01:23:01 +0300

    amri

--- pu_graphs/evaluation/evaluation.py
+++ pu_graphs/evaluation/evaluation.py
@@ -6,7 +6,7 @@ import dgl
 import scipy
 import torch
 from catalyst.metrics import IMetric, MRRMetric, AccuracyMetric, AdditiveMetric, ICallbackBatchMetric, \
-    process_recsys_components
+    process_recsys_components, FunctionalBatchMetric
 from torch.utils.data import DataLoader
 from tqdm import tqdm
 
@@ -14,7 +14,7 @@ from tqdm import tqdm
 class LinkPredictionMetric(IMetric, abc.ABC):
 
     @abc.abstractmethod
-    def update(self, head_idx, tail_idx, logits, graph) -> Any:
+    def update(self, head_idx, tail_idx, logits) -> Any:
         pass
 
 
@@ -29,15 +29,15 @@ class LinkPredictionMetricAdapter(LinkPredictionMetric):
 
     # FIXME: probably you don't need graph parameter since you use only to infer number of nodes,
     # this info is already contained in logits
-    def update(self, head_idx, tail_idx, logits, graph) -> Any:
-        targets = self._transform_targets(targets=tail_idx, graph=graph)
+    def update(self, head_idx, tail_idx, logits) -> Any:
+        targets = self._transform_targets(head_idx=head_idx, tail_idx=tail_idx, logits=logits)
         return self.metric.update(logits=logits, targets=targets)
 
     def compute(self) -> Any:
         return self.metric.compute()
 
-    def _transform_targets(self, targets, graph):
-        return targets
+    def _transform_targets(self, head_idx, tail_idx, logits):
+        return tail_idx
 
     def __getattr__(self, item):
         return getattr(self.metric, item)
@@ -51,7 +51,7 @@ class FilteredLinkPredictionMetric(LinkPredictionMetricAdapter):
         self.full_adj_mat = full_adj_mat
 
     @staticmethod
-    def filter_logits(full_adj_mat, logits, head_idx, tail_idx, graph):
+    def filter_logits(full_adj_mat, logits, head_idx, tail_idx):
         """
         Makes logits for pairs other than (head_idx, tail_idx)
         """
@@ -67,11 +67,11 @@ class FilteredLinkPredictionMetric(LinkPredictionMetricAdapter):
             logits
         )
 
-    def update(self, head_idx, tail_idx, logits, graph) -> Any:
+    def update(self, head_idx, tail_idx, logits) -> Any:
         logits = FilteredLinkPredictionMetric.filter_logits(
-            full_adj_mat=self.full_adj_mat, logits=logits, head_idx=head_idx, tail_idx=tail_idx, graph=graph
+            full_adj_mat=self.full_adj_mat, logits=logits, head_idx=head_idx, tail_idx=tail_idx
         )
-        return self.metric.update(logits=logits, head_idx=head_idx, tail_idx=tail_idx, graph=graph)
+        return self.metric.update(logits=logits, head_idx=head_idx, tail_idx=tail_idx)
 
 
 class MRRLinkPredictionMetric(LinkPredictionMetricAdapter):
@@ -79,8 +79,9 @@ class MRRLinkPredictionMetric(LinkPredictionMetricAdapter):
     def __init__(self, topk_args=None):
         super(MRRLinkPredictionMetric, self).__init__(metric=MRRMetric(topk_args=topk_args))
 
-    def _transform_targets(self, targets, graph):
-        return torch.nn.functional.one_hot(targets, num_classes=graph.number_of_nodes())
+    def _transform_targets(self, head_idx, tail_idx, logits):
+        number_of_nodes = logits.shape[-1]
+        return torch.nn.functional.one_hot(tail_idx, num_classes=number_of_nodes)
 
 
 class AccuracyLinkPredictionMetric(LinkPredictionMetricAdapter):
@@ -144,37 +145,86 @@ def realistic_rank(logits: torch.Tensor, target_idx: torch.Tensor, k: int):
 
 # this one is used by catalyst metrics
 def non_deterministic_rank(logits: torch.Tensor, target_idx: torch.Tensor, k: int):
+    """
+    Rank is 0 when it is larger than k
+    """
     targets = torch.nn.functional.one_hot(target_idx, num_classes=logits.shape[-1])
     sorted_targets = process_recsys_components(outputs=logits, targets=targets)[:, :k]
+
+    values, indices = torch.max(sorted_targets, dim=1)
+    indices += 1
+    indices[values == 0] = 0
+
+    return indices
 
 
 class AdjustedMeanRankIndex(LinkPredictionMetric, ICallbackBatchMetric):
 
-    def __init__(self,
+    def __init__(
+        self,
         topk_args: ty.List[int] = None,
         compute_on_call: bool = True,
         prefix: str = None,
-        suffix: str = None
+        suffix: str = None,
+        rank_fn: ty.Optional[ty.Callable] = None
     ):
         ICallbackBatchMetric.__init__(self, compute_on_call=compute_on_call, prefix=prefix, suffix=suffix)
-        self.metric_name_mean = f"{self.prefix}mrr{self.suffix}"
-        self.metric_name_std = f"{self.prefix}mrr{self.suffix}/std"
-        self.topk_args: ty.List[int] = topk_args or [1]
-        self.additive_metrics: ty.List[AdditiveMetric] = [
-            AdditiveMetric() for _ in range(len(self.topk_args))
+        self.metric_name_mean = f"{self.prefix}amri{self.suffix}"
+        self.metric_name_std = f"{self.prefix}amri{self.suffix}/std"
+        self.topk_args: ty.List[int] = sorted(topk_args or [1])
+        self.rank_fn = rank_fn if rank_fn is not None else non_deterministic_rank
+        self.metrics = [
+             FunctionalBatchMetric(
+                 AdjustedMeanRankIndex.get_amri_at_k_fn(self.rank_fn, k),
+                 metric_key=f"amri_{k}",
+                 prefix=prefix,
+                 suffix=suffix
+             ) for k in self.topk_args
         ]
 
-    def update(self, head_idx, tail_idx, logits, graph) -> Any:
-        pass
+    @staticmethod
+    def get_amri_at_k_fn(rank_fn: ty.Callable, k: int):
+        def metric_fn(tail_idx, logits):
+            ranks = rank_fn(logits=logits, target_idx=tail_idx, k=k)
+            n_tails = logits.shape[-1]
+            ranks[ranks == 0] = n_tails
+            value = (2 / n_tails) * ranks.sub(1).sum()
+            return value
+        return metric_fn
+
+    def update(self, head_idx, tail_idx, logits) -> Any:
+        return [
+            m.update(
+                batch_size=logits.shape[0],
+                tail_idx=tail_idx,
+                logits=logits
+            )
+            for m in self.metrics
+        ]
 
     def reset(self) -> None:
-        pass
+        for m in self.metrics:
+            m.reset()
 
     def compute(self) -> Any:
-        pass
+        means, stds = zip(*(metric.compute() for metric in self.metrics))
+        return means, stds
 
-    def update_key_value(self, *args, **kwargs) -> Dict[str, float]:
-        pass
+    def update_key_value(self, head_idx, tail_idx, logits) -> Dict[str, float]:
+        values = self.update(head_idx, tail_idx, logits)
+        output = {
+            m.metric_name: v
+            for m, v in zip(self.metrics, values)
+        }
+        output[self.metric_name_mean] = output[self.metrics[-1].metric_name]
+        return output
 
     def compute_key_value(self) -> Dict[str, float]:
-        pass
+        values = {}
+        for m in self.metrics:
+            values.update(m.compute_key_value())
+
+        values[self.metric_name_mean] = values[self.metrics[-1].metric_name]
+        values[self.metric_name_std] = values[f"{self.metrics[-1].metric_name}/std"]
+
+        return values
