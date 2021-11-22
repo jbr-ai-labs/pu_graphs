fe1588910423f4f46827ea5ba707919aff6e3410
Dima <d.i.orekhov@gmail.com>
2021-11-22 23:34:56 +0300

    log WIP

--- pu_graphs/main.py
+++ pu_graphs/main.py
@@ -1,4 +1,5 @@
 import os
+from copy import deepcopy
 from functools import partial
 from pathlib import Path
 
@@ -16,7 +17,7 @@ from pu_graphs.data.negative_sampling import UniformStrategy
 from pu_graphs.data.utils import get_split
 from pu_graphs.debug_utils import DebugDataset
 from pu_graphs.evaluation.evaluation import compute_score_based_metrics_for_loader, MRRLinkPredictionMetric, \
-    AccuracyLinPredictionMetric
+    AccuracyLinkPredictionMetric, AdjustedMeanRankIndex, compute_score_based_metrics_for_loader_optimized
 from pu_graphs.modeling.dist_mult import DistMult
 from pu_graphs.modeling.loss import UnbiasedPULoss, sigmoid_loss, logistic_loss
 
@@ -24,8 +25,8 @@ from pu_graphs.modeling.loss import UnbiasedPULoss, sigmoid_loss, logistic_loss
 def main():
     config = load_config("config.yaml")  # FIXME: replace hardcode
 
-    if config["is_debug"]:
-        os.environ["WANDB_MODE"] = "dryrun"
+    #if config["is_debug"]:
+    #    os.environ["WANDB_MODE"] = "dryrun"
 
     set_global_seed(config["seed"])
 
@@ -79,6 +80,27 @@ def main():
         batch.update(new_fields)
         return batch
 
+
+    class EvaluationCallback(dl.Callback):
+        def __init__(self, loader_key: str):
+            self.loader_key = loader_key
+            super(EvaluationCallback, self).__init__(order=dl.CallbackOrder.ExternalExtra, node=dl.CallbackNode.Master)
+
+        def on_experiment_end(self, runner: "IRunner") -> None:
+            metrics = {"mrr": 10.0}
+
+            kwargs = deepcopy(runner._log_defaults)
+            kwargs.update({
+                "metrics": metrics,
+                "scope": "loader",
+                "loader_key": self.loader_key
+            })
+
+            for logger in runner.loggers.values():
+                logger.log_metrics(**kwargs)
+
+
+    logdir = Path("./logdir") / config["run_name"]
     callbacks = [
         dl.BatchTransformCallback(
             transform=transform_as_pos_neg,
@@ -89,7 +111,15 @@ def main():
             loader_key="valid",
             metric_key="loss",  # TODO: replace with MRR,
             minimize=True
-        )
+        ),
+        dl.CheckpointCallback(
+            logdir=logdir.joinpath("checkpoints"),
+            loader_key="valid",
+            metric_key="loss",
+            minimize=True,
+            load_on_stage_end="best"
+        ),
+        EvaluationCallback("test")
     ]  # TODO: init
 
     loggers = {
@@ -111,7 +141,7 @@ def main():
         loaders=train_and_valid_loaders,
         callbacks=callbacks,
         verbose=True,
-        logdir=Path("./logdir") / config["run_name"],
+        logdir=logdir,
         num_epochs=config["num_epochs"],
         check=config["is_debug"],
         load_best_on_end=True,
@@ -120,17 +150,18 @@ def main():
     )
 
     # TODO: move inside runner
-    for split_key in ("valid", "test"):
-        metrics = compute_score_based_metrics_for_loader(
-            graph=graphs[split_key],
-            model=model,
-            loader=loaders[split_key],
-            metrics={
-                "mrr": MRRLinkPredictionMetric(topk_args=[full_graph.number_of_nodes()]),
-                "acc": AccuracyLinPredictionMetric(topk_args=[1, 3, 5, 10, 20])
-            }
-        )
-        print(split_key, metrics)
+    #for split_key in ("valid", "test"):
+    #    metrics = compute_score_based_metrics_for_loader_optimized(
+    #        graph=graphs[split_key],
+    #        model=model,
+    #        loader=loaders[split_key],
+    #        metrics={
+    #            "mrr": MRRLinkPredictionMetric(topk_args=[full_graph.number_of_nodes()]),
+    #            "acc": AccuracyLinkPredictionMetric(topk_args=[1, 3, 5, 10, 20]),
+    #            "amri": AdjustedMeanRankIndex(topk_args=[full_graph.number_of_nodes()])
+    #        }
+    #    )
+    #    print(split_key, metrics)
 
 
 if __name__ == '__main__':
